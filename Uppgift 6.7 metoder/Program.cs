590ee8983a65f99bbbaae5324f4c030cf21bfea1
HaZ3R0 <megamackan@outlook.com>
2024-03-01 12:15:53 +0100

    Add project files.

---
+++ Uppgift 6.7 metoder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace primtal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal du vill under söka");
+            int tal = int.Parse(Console.ReadLine());
+            Primtal(tal);
+        }
+
+        static void Primtal(int tal)
+        {
+            int deltal = 0;
+            for (int i = 2; i < tal; i++)
+            {
+                if (tal % i == 0)
+                {
+                    deltal++;
+                }
+            }
+            if (deltal == 0)
+            {
+                Console.WriteLine("Ditt tal är ett primtal");
+            }
+            else
+            {
+                Console.WriteLine("Ditt tal är inte ett primtal");
+            }
+        }
+    }
+}
